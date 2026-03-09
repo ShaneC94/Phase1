dcb19d944fd9bdd93743f5b482a135d84196fa6c
ShaneC94 <103201687+ShaneC94@users.noreply.github.com>
2026-03-09 11:21:34 -0400

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,20 +1,25 @@
 import csv
 
 def parse_row(line):
-    cols = line.split(',')
-    
+
+    cols = next(csv.reader([line]))
+
+    if len(cols) < 15:
+        return None
+
     try:
         return {
-            'vehicle_id': int(cols[0]),
-            'frame_id': int(cols[1]),
-            'local_x': float(cols[4]),
-            'local_y': float(cols[5]),
-            'velocity': float(cols[11]),
-            'acceleration': float(cols[12]),
-            'lane_id': int(cols[13]),
-            'preceding': int(cols[14]),
+            "vehicle_id": int(cols[0]),
+            "frame_id": int(cols[1]),
+            "local_x": float(cols[4]),
+            "local_y": float(cols[5]),
+            "velocity": float(cols[11]),
+            "acceleration": float(cols[12]),
+            "lane_id": int(cols[13]),
+            "preceding": int(cols[14]),
         }
-    except:
+
+    except ValueError:
         return None
     
 def remove_invalid(row):
