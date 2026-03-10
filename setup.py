f85d18b6950a7a3b77beea9fb4494d9f3a523c23
ShaneC94 <103201687+ShaneC94@users.noreply.github.com>
2026-03-10 11:16:12 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name="ngsim_pipeline",
     version="0.1",
+    install_requires=[
+        "apache-beam[gcp]"
+    ],
     packages=find_packages(),
-    py_modules=["pipeline", "preprocessing", "scenarios"],
+    py_modules=[
+        "pipeline",
+        "scenario",
+        "preprocessing",
+        "visualization"
+    ]
 )
